737df4b4fed4bc6122c0bd5dac121f654c5a9151
ChiefLopes <chieflopesekede@gmail.com>
2023-06-15 11:21:21 +0100

    added a restaurant object

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const restaurant = {
-
-    
-}
+  name: "Classico  Italiano",
+  location: "Via Angela Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focazzia", "Bruscheta", "Garlic Bread", "Capric Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+};
